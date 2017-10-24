6bfd7b6b811ceac7e29f6258499e6994d7a57199
jasonm718 <jasonm718@gmail.com>
2017-10-24 18:47:17 +0000

    Reading from arduino

--- webApp/sense.py
+++ webApp/sense.py
@@ -2,8 +2,19 @@
 #This is where the code to speak to the sensors will be written
 
 import RPi.GPIO as GPIO
+import serial
 
-class sensors(object):
+arduinoData = serial.Serial('/dev/ttyACM1',9600)
+
+while (1==1):
+    if(arduinoData.inWaiting() > 0):
+        myData = arduinoData.readline()
+        print (myData)
+
+
+"""class sensors(object):
 	
 	def __inti__(self):
 		GPIO.setwarnings(False)
+
+"""
