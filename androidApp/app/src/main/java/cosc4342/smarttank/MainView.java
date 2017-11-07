7853d479da56331d763436626fedd2d737f912ee
Cesar Salazar <cesarsalazar09@gmail.com>
2017-11-07 17:13:33 -0600

    added android app code

---
+++ androidApp/app/src/main/java/cosc4342/smarttank/MainView.java
@@ -0,0 +1,13 @@
+package cosc4342.smarttank;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainView extends AppCompatActivity {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_view);
+    }
+}
