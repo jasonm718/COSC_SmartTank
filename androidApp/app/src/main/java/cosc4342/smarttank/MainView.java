2198416b6ac61eb92ffafac020d75e6ef89d3fea
Cesar Salazar <cesarsalazar09@gmail.com>
2017-11-09 21:24:24 -0600

    fleshed out the simple ui with a listview of sensors and dummy images that represent states

--- androidApp/app/src/main/java/cosc4342/smarttank/MainView.java
+++ androidApp/app/src/main/java/cosc4342/smarttank/MainView.java
@@ -1,37 +1,31 @@
 package cosc4342.smarttank;
 
-import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.security.AccessController.getContext;
-
 public class MainView extends AppCompatActivity {
     
-    GridView sensorsGrid;
     SensorAdapter sensors;
+    ListView sensors_list;
     
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_view);
-        
-        sensorsGrid = (GridView) findViewById(R.id.sensors_grid);
+    
+        sensors_list = (ListView) findViewById(R.id.notification_list);
         sensors = new SensorAdapter();
         populate(sensors);
-        sensorsGrid.setAdapter(sensors);
+        sensors_list.setAdapter(sensors);
     }
     
     
@@ -43,6 +37,7 @@ public class MainView extends AppCompatActivity {
         sensors.add("Ammonia");
         sensors.add("Salinity");
         sensors.add("Nitrates");
+        sensors.add("Water Level");
         
         list.setSensors(sensors);
     }
@@ -56,7 +51,7 @@ public class MainView extends AppCompatActivity {
         }
     }
     
-    private class SensorAdapter extends BaseAdapter{
+    class SensorAdapter extends BaseAdapter{
         List<String> sensors;
     
         public void setSensors(List<String> sensors) {
@@ -83,7 +78,7 @@ public class MainView extends AppCompatActivity {
             SensorHolder holder;
             if(row == null){
                 
-                row = getLayoutInflater().inflate(R.layout.sensor, viewGroup, false);
+                row = getLayoutInflater().inflate(R.layout.sensor_row, viewGroup, false);
                 holder = new SensorHolder(row);
                 row.setTag(holder);
             }else{
