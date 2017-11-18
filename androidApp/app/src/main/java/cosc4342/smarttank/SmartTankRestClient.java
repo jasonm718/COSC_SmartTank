7c647710e2deab99d5d1673a8600a553b09924e0
Cesar Salazar <cesarsalazar09@gmail.com>
2017-11-18 13:19:19 -0600

    added synchronous http client

--- androidApp/app/src/main/java/cosc4342/smarttank/SmartTankRestClient.java
+++ androidApp/app/src/main/java/cosc4342/smarttank/SmartTankRestClient.java
@@ -4,20 +4,43 @@ package cosc4342.smarttank;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.SyncHttpClient;
 import com.loopj.android.http.TextHttpResponseHandler;
+
+import cz.msebera.android.httpclient.client.HttpClient;
 
 public class SmartTankRestClient {
     
     private static final String BASE_URL = "https://smarttank.herokuapp.com/";
     
-    private static AsyncHttpClient client = new AsyncHttpClient();
+    private static AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+    private static SyncHttpClient syncHttpClient = new SyncHttpClient();
+    private static boolean async;
+    
+    public static void setAsync(boolean async){
+        if(async){
+            SmartTankRestClient.async = true;
+        } else {
+            SmartTankRestClient.async = false;
+        }
+        
+    }
     
     public static void get(String url, AsyncHttpResponseHandler responseHandler) {
-        client.get(getAbsoluteUrl(url), responseHandler);
+        
+        if(SmartTankRestClient.async) {
+            asyncHttpClient.get(getAbsoluteUrl(url), responseHandler);
+        }else{
+            syncHttpClient.get(getAbsoluteUrl(url), responseHandler);
+        }
     }
     
     public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        client.post(getAbsoluteUrl(url), params, responseHandler);
+        if(SmartTankRestClient.async) {
+            asyncHttpClient.post(getAbsoluteUrl(url), params, responseHandler);
+        }else {
+            syncHttpClient.post(getAbsoluteUrl(url), params, responseHandler);
+        }
     }
     
     public static String getAbsoluteUrl(String relativeUrl) {
